505c2435516bc95b94680ee1a3926c581bcda376
claratesmon <claraperezmontes@gmail.com>
2023-11-27 09:01:55 +0100

    a bit of css

--- src/pages/AllBeersPage.jsx
+++ src/pages/AllBeersPage.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import { useState, useEffect } from "react";
+import "/public/allbeerspage.css"
 
 
 function AllBeersPage() {
@@ -21,21 +22,30 @@ function AllBeersPage() {
       console.log(error);
     }
   }
+
+  const handleSearch = async () => {
+    
+  }
   return (
     <>
 
       <h1>ALL BEERS</h1>
+      <div>
+        <input onChange={(event) => handleSearch(event.target.value)} id="search-input" type="search" placeholder="Search Beer" /><span id="magni-icon">🔍</span>
+      </div>
       <ul>
         {
           beers.map((eachBeer) => {
             console.log(eachBeer._id)
             return (
               <Link to={`/beers/${eachBeer._id}`}>
-                <div key={eachBeer._id}>
-                  <h2>{eachBeer.name}</h2>
-                  <img src={eachBeer.image_url} alt={eachBeer.name} />
-                  <p>{eachBeer.tagline}</p>
-                  <p>{eachBeer.contributed_by}</p>
+                <div className="beer-container" key={eachBeer._id}>
+                  <img id="allbeers-img" src={eachBeer.image_url} alt={eachBeer.name} />
+                  <div className="beers-info">
+                  <h4>{eachBeer.name}</h4>
+                  <p className="tagline" >{eachBeer.tagline}</p>
+                  <p className="contributed" >{eachBeer.contributed_by}</p>
+                  </div>
                 </div>
               </Link>
 
